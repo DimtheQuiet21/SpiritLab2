8347e16d95dc4341922d6294b42deb1e7725c57f
Abe <abe.wav.bis@gmail.com>
2024-02-15 21:19:13 -0600

    Added MUI Packages/Imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
