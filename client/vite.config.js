825c3562f7ae2bc0ae6f9bc9ab17249ddd6c6f78
Abe <abe.wav.bis@gmail.com>
2024-02-21 21:23:44 -0600

    Fixed Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     open: true
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly'
+    }
   }
-})
+});
