64adbcfe551878124b4c493d7375f2cdd1cd9b39
Abe <abe.wav.bis@gmail.com>
2024-02-29 16:53:32 -0600

    Welcome updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,23 +3,27 @@ import Welcome from "../components/home/Welcome";
 import PopularDrink from "../components/home/PopularDrink";
 import DrinkOfDay from "../components/home/DrinkOfDay";
 import RandomGen from '../components/home/RandomGen';
+import { Container } from '@mui/material';
 
 function Home() {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-                <Welcome />
+        <Container maxWidth='xl'>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Welcome />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <DrinkOfDay />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <PopularDrink />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <RandomGen />
+                </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-                <DrinkOfDay />
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <PopularDrink />
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <RandomGen />
-            </Grid>
-        </Grid>
+        </Container>
+
     );
 }
 
